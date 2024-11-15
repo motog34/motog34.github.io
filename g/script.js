5762bf05140b7645f271bfa2b5c0239f9aea58c0
Uneike <vinidrika002@gmail.com>
2024-11-15 11:50:44 -0300

    Update script.js

--- g/script.js
+++ g/script.js
@@ -47,7 +47,7 @@ function loadPost() {
     const fileName = urlParams.get('post');
 
     if (fileName) {
-        fetch(`./posts/${fileName}.md`)
+        fetch(`./g/posts/${fileName}.md`)
             .then(response => {
                 if (!response.ok) {
                     window.location.href = "404.html";
@@ -59,7 +59,7 @@ function loadPost() {
                 const authorInfo = extractAuthorInfo(md);
                 const postHTML = markdownToHTML(md.replace(/Info {[^}]+}/, '')); // Remove author info from markdown
                 const postContainer = document.getElementById('post-content');
-                const postDate = getFileDate(`./posts/${fileName}.md`);
+                const postDate = getFileDate(`./g/posts/${fileName}.md`);
 
                 // Add author info to the post
                 let authorHTML = '';
